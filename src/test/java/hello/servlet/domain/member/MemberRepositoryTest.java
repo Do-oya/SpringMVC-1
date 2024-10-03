a6ea52622ee70912f58c73a309f4ddcb4b14cb8b
Do-oya <qkrwjdgus1441@naver.com>
2024-10-03 19:44:58 +0900

    [김영한 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술]

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    @DisplayName("저장 테스트")
+    public void save() {
+        // given
+        Member member = new Member("dooya", 20);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    @DisplayName("조회 테스트")
+    public void findAll() {
+        // given
+        Member member1 = new Member("dooya1", 20);
+        Member member2 = new Member("dooya2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
